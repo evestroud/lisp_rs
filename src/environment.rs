7405da555e2aa50947abbbb453a188046dc9f232
Eve Stroud <evestroud89@gmail.com>
2022-11-25 20:43:28 -0500

    Add mul and div builtins

--- src/environment.rs
+++ src/environment.rs
@@ -16,6 +16,8 @@ impl Env {
             table: HashMap::from([
                 ("+".to_string(), Atom::Builtin(Rc::from(Builtin(&add)))),
                 ("-".to_string(), Atom::Builtin(Rc::from(Builtin(&sub)))),
+                ("*".to_string(), Atom::Builtin(Rc::from(Builtin(&mul)))),
+                ("/".to_string(), Atom::Builtin(Rc::from(Builtin(&div)))),
             ]),
             parent: None,
         }
@@ -27,26 +29,12 @@ impl Env {
 }
 
 fn add(args: Vec<Atom>) -> Result<Atom, EvalError> {
-    let mut result;
-    match args.get(0) {
-        Some(val) => {
-            result = match val {
-                Atom::Int(num) => *num,
-                Atom::Float(_) => todo!(),
-                _ => return Err(EvalError(format!("Expected a number, found {:?}", val))),
-            }
-        }
-        None => {
-            return Err(EvalError(
-                "+ expects at least one argument, found none".to_string(),
-            ))
-        }
-    }
-
-    for item in args[1..].iter() {
+    let mut result = 0;
+    for item in args {
         match item {
             Atom::Int(val) => result += val,
-            _ => todo!(),
+            Atom::Float(_) => todo!(),
+            _ => return Err(EvalError(format!("Expected a number, found {:?}", item))),
         }
     }
     Ok(Atom::Int(result))
@@ -76,8 +64,39 @@ fn sub(args: Vec<Atom>) -> Result<Atom, EvalError> {
     for item in args[1..].iter() {
         match item {
             Atom::Int(val) => result -= val,
-            _ => todo!(),
+            _ => return Err(EvalError(format!("Expected a number, found {:?}", item))),
         }
     }
     Ok(Atom::Int(result))
+}
+
+fn mul(args: Vec<Atom>) -> Result<Atom, EvalError> {
+    let mut result = 1;
+    for item in args {
+        match item {
+            Atom::Int(val) => result *= val,
+            Atom::Float(_) => todo!(),
+            _ => return Err(EvalError(format!("Expected a number, found {:?}", item))),
+        }
+    }
+    Ok(Atom::Int(result))
+}
+
+fn div(args: Vec<Atom>) -> Result<Atom, EvalError> {
+    if args.len() != 2 {
+        return Err(EvalError(format!(
+            "/ takes 2 arguments, found {}",
+            args.len()
+        )));
+    }
+    let mut args = args.iter();
+    let num = match args.next().unwrap() {
+        Atom::Int(val) => *val as f32,
+        _ => todo!(),
+    };
+    let den = match args.next().unwrap() {
+        Atom::Int(val) => *val as f32,
+        _ => todo!(),
+    };
+    Ok(Atom::Float(num / den))
 }
