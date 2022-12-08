fc296fcc1994d22beafb420c2feaaecd5479c93e
Eve Stroud <evestroud89@gmail.com>
2022-12-08 07:59:38 -0500

    Integrate Buffer into parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,38 +1,39 @@
 use crate::{
+    buffer::Buffer,
     lib::SchemeError,
     tokenizer::Token,
     types::{Exp, Value},
 };
 use std::collections::VecDeque;
 
-pub(crate) fn parse_all(tokens: &mut VecDeque<Token>) -> Result<Vec<Exp>, SchemeError> {
+pub(crate) fn parse_all(buffer: &mut Buffer) -> Result<Vec<Exp>, SchemeError> {
     let mut result = Vec::new();
-    while tokens.len() != 0 {
-        result.push(parse(tokens)?);
+    while buffer.len() != 0 {
+        result.push(parse(buffer)?);
     }
     Ok(result)
 }
 
-pub(crate) fn parse(tokens: &mut VecDeque<Token>) -> Result<Exp, SchemeError> {
-    if tokens.len() == 0 {
+pub(crate) fn parse(buffer: &mut Buffer) -> Result<Exp, SchemeError> {
+    if buffer.len() == 0 {
         return Err(SchemeError("Unexpected EOF while parsing".to_string()));
     }
-    let t = &tokens.pop_front().unwrap();
+    let t = &buffer.pop_front().unwrap();
     match t {
         Token::StartExp => {
             let mut exp = Vec::new();
-            while tokens
+            while buffer
                 .front()
                 .ok_or(SchemeError("Unexpected EOF while parsing".to_string()))?
                 != &Token::EndExp
             {
-                exp.push(parse(tokens)?);
+                exp.push(parse(buffer)?);
             }
-            tokens.pop_front();
+            buffer.pop_front();
             return Ok(Exp::List(exp));
         }
         Token::EndExp => Err(SchemeError("Unmatched ')'".to_string())),
-        Token::Quote => Ok(Exp::Atom(Value::Quote(Box::from(parse(tokens)?)))),
+        Token::Quote => Ok(Exp::Atom(Value::Quote(Box::from(parse(buffer)?)))),
         Token::Literal(value) => Ok(Exp::Atom(value.clone())),
     }
 }
