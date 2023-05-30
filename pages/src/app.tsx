92902c2e496d0d1421e62e748e23b3d4b26d6ea4
Eve Stroud <evestroud89@gmail.com>
2023-05-29 20:31:51 -0400

    Link wasm to vite package

--- pages/src/app.tsx
+++ pages/src/app.tsx
@@ -1,8 +1,11 @@
 import { useMemo } from "preact/hooks";
 import Terminal from "./Terminal";
 import "./app.css";
+import init from "../../target/wasm32-unknown-unknown/debug/lisp_rs.wasm?init";
 
 export function App() {
+  init({ help: { no: 0 } }).then((x) => console.log(x));
+
   const worker = useMemo(
     () => new Worker(new URL("./worker.ts", import.meta.url)),
     []
