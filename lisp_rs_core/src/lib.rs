a9fc1fc2b1e81be5635a41f4131fcbe3bbc6ac0e
Eve Stroud <evestroud89@gmail.com>
2024-06-06 23:48:58 -0400

    Combine Cell & Value into one enum

--- lisp_rs_core/src/lib.rs
+++ lisp_rs_core/src/lib.rs
@@ -4,30 +4,54 @@ pub mod lexer;
 pub mod parser;
 
 pub mod types {
+    use std::fmt::Display;
+
     #[derive(PartialEq, Eq, Clone, Debug)]
-    pub struct Cell {
-        pub car: Value,
-        pub cdr: Value,
+    pub enum Cell {
+        Nil,
+        Pair(Box<Cell>, Box<Cell>),
+        Number(i32), // TODO Numeric tower
+        Boolean(bool),
+        Symbol(String),
+        Quote(Box<Cell>),
+        // TODO String(String)
+    }
+
+    impl Cell {
+        pub fn new_pair() -> Self {
+            Cell::Pair(Box::new(Cell::Nil), Box::new(Cell::Nil))
+        }
     }
 
     impl Default for Cell {
         fn default() -> Self {
-            Cell {
-                car: Value::Nil,
-                cdr: Value::Nil,
-            }
+            Cell::Nil
         }
     }
 
-    #[derive(PartialEq, Eq, Clone, Debug)]
-    pub enum Value {
-        Nil,
-        List(Box<Cell>),
-        Number(i32), // TODO Numeric tower
-        Boolean(bool),
-        Symbol(String),
-        Quote(Box<Value>),
-        // TODO String(String)
+    impl Display for Cell {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(
+                f,
+                "{}",
+                match self {
+                    Cell::Nil => "".to_owned(),
+                    Cell::Pair(car, cdr) => {
+                        let list = vec![car.to_string()];
+                        let tail = cdr;
+                        while let Cell::Pair(ref _car, ref _cdr) = **tail {
+                            // TODO add cars to list
+                        }
+                        // TODO if remaining cdr is not Nil, add a dot and final value
+                        format!("({})", list.join(" "))
+                    }
+                    Cell::Number(n) => format!("{}", n),
+                    Cell::Boolean(b) => format!("{}", b),
+                    Cell::Symbol(s) => format!("{}", s),
+                    Cell::Quote(q) => todo!(),
+                }
+            )
+        }
     }
 
     #[cfg(test)]
